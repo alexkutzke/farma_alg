95582dd1059a941627358321623a48c262e949d0
Alex Kutzke <alexkutzke@gmail.com>
2014-02-11 10:57:38 -0200

    Using numdiff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Carrie::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application2.js panel/*.js )
+  config.assets.precompile += %w( application2.js panel/*.js panel.js )
   config.assets.precompile += Ckeditor.assets
 
   # Disable delivery errors, bad email addresses will be ignored
