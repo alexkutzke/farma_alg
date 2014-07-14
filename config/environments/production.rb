ce57c201b59635ebedea7495d03b77e95feff294
Alex Kutzke <alexkutzke@gmail.com>
2014-07-14 16:34:57 -0300

    scripts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,9 @@ Carrie::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( application2.js panel/*.js panel.js login.js dashboard/*.js )
-  config.assets.precompile += Ckeditor.assets
+  #config.assets.precompile += %w( application2.js panel/*.js panel.js login.js dashboard/*.js )
+  #config.assets.precompile += Ckeditor.assets
+  config.assets.precompile = ['*.js', '*.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
