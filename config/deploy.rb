56509d851b49ba93e6ff109bcb81b4d9b592795d
Alex Kutzke <alexkutzke@gmail.com>
2014-07-15 10:43:21 -0300

    scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,16 +50,32 @@ namespace :deploy do
 
   task :stop do ; end
 
+  #namespace :assets do
+  #  task :precompile, :roles => :web, :except => { :no_release => true } do
+  #     from = source.next_revision(current_revision)
+  #     if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+  #     else
+  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
+  #     end
+  # end
+  #end
+
+namespace :deploy do
   namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
     task :precompile, :roles => :web, :except => { :no_release => true } do
-       from = source.next_revision(current_revision)
-       if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-       else
-         logger.info "Skipping asset pre-compilation because there were no asset changes"
-       end
-   end
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        %x{bundle exec rake assets:precompile}
+        %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+        %x{bundle exec rake assets:clean}
+      else
+        logger.info 'Skipping asset pre-compilation because there were no asset changes'
+      end
+    end
   end
+end 
 
   desc "Create ckeditor link"
   task :ckeditor_link do
