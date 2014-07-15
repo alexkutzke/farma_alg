96af5ee815825358227a3758047820eda0fbaf91
Alex Kutzke <alexkutzke@gmail.com>
2014-07-15 14:15:29 -0300

    scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -66,7 +66,7 @@ namespace :deploy do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
       if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        %x{bundle exec rake assets:precompile}
+        %x{RAILS_ENV=production RAILS_GROUPS=assets bundle exec rake assets:precompile}
         %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
         %x{bundle exec rake assets:clean}
       else
